18ae3e7ffb1e6de7381aa8a6494ba0811ec11402
Nizar Noorani <nizar.noorani@gmail.com>
2013-06-13 19:50:25 -0500

    makes attributes accessible for invoice and invoice_item

--- app/models/spree/invoice_item.rb
+++ app/models/spree/invoice_item.rb
@@ -1,5 +1,7 @@
 module Spree
   class InvoiceItem < ActiveRecord::Base
+    attr_accessible :product_id, :quantity, :line_item_id
+    
     belongs_to :supplier_invoice
     belongs_to :line_item
   end
--- app/models/spree/supplier_invoice.rb
+++ app/models/spree/supplier_invoice.rb
@@ -1,5 +1,7 @@
 module Spree
   class SupplierInvoice < ActiveRecord::Base
+    attr_accessible :order_id, :supplier_id, :item_count
+    
     belongs_to :supplier
     has_many :invoice_items
   end
